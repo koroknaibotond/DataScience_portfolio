2fbb5ee0dff2b0842f9db93bf440a41af02473c4
Botond Koroknai <127404741+koroknaibotond@users.noreply.github.com>
2026-03-15 17:00:31 +0100

    Refactor Monte Carlo Integration with class parameters
    Updated Monte Carlo Integration implementation to use a parameterized class for better structure and added result retrieval method.

--- Numerical_methods/Monte_Carlo_methods/Monte_Carlo_Integration/result.cpp
+++ Numerical_methods/Monte_Carlo_methods/Monte_Carlo_Integration/result.cpp
@@ -1,16 +1,20 @@
-#include "montecarlo.h"
 #include <iostream>
+#include "montecarlo.hpp"
 
 int main()
 {
-    double result = MonteCarlo(
+    MonteCarlo params(
         [](double x, double y, double z)
         { return std::exp(-x * x - y * y - z * z); },
         [](double x, double y, double z) -> bool
         { return x * x + y * y + z * z < 16.0; },
-        -4.0, 4.0, -4.0, 4.0, -4.0, 4.0);
+        -4.0, 4.0, -4.0, 4.0, -4.0, 4.0, 2e6); // We set the parameters for the Monte Carlo Integration
 
-    std::cout << "The result of the integration: " << result << std::endl;
+    double result = params.getResult(); // With the help of this function we call the result of the function from the private part of the class. ezzel a segédfüggvénnyel előhívjuk a class, private részéből az inegrálás eredményét.
+    std::cout << "\n";
+
+    std::cout << "Az integrálás eredménye: " << result << std::endl; // We print out the result
+    std::cout << "\n";
 
     return 0;
 }
